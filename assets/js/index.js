0ff01737759b71ad6c8d1bb30c3d39d56b87b9a9
Christian <christian.sulecki@gmail.com>
2022-10-07 17:02:38 +0200

    improving stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,90 @@
-const fetchKeywords = (url) => {
+const fetchKeywords = (url, callback) => {
 
-	var toto = fetch(`https://rxga0voqfh.execute-api.eu-west-3.amazonaws.com/prod?uri=${encodeURIComponent(url)}`)
-//	console.log(toto);
-	
-	fetch(url)
-		.then(function() {
-			
+	const wrappedCallback = () => {
+		if(callback && typeof callback === "function") {
+			callback();
+		}
+	}
+
+	fetch(`https://rxga0voqfh.execute-api.eu-west-3.amazonaws.com/prod?uri=${encodeURIComponent(url)}`)
+		.then((response) => {
+			response.json()
+				.then((data) => {
+					wrappedCallback();
+					console.log(data);
+				})
+				.catch(err => {
+					wrappedCallback();
+					console.log(`error: ${err}`);					
+				});
 		})
 		.catch(err => {
+			wrappedCallback();
 			console.log(`error: ${err}`);
-		})
+		});
 };
+
+const setNewUrlParameter = (value) => {
+	const newUrl = new URL(window.location);
+
+	if(value) {
+		newUrl.searchParams.set('url', value);
+	} else {
+		newUrl.searchParams.delete('url');
+	}
+	
+	window.history.pushState({}, '', newUrl);
+}
 
 const startKeywordFinder = (event) => {
 
-	event.preventDefault();
+	if(event) {
+		event.preventDefault();
+	}
 
-	let urlInput = document.querySelector("input[name=url]");
+	const urlInput = document.querySelector("input[name=url]");
 	let urlValue = urlInput.value;
 
 	if(urlValue && !urlValue.match(/^http/)) {
 		urlValue = `https://${urlValue}`;
 	}
 
-	let isValidUrl = urlValue.match(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/);
+	const isValidUrl = urlValue.match(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/);
 
 	if(!urlValue || !isValidUrl) {
 		return urlInput.classList.add("error");
 	}
 
 	urlInput.classList.remove("error");
+	urlInput.classList.add("loading");
 
-	fetchKeywords(urlValue);
+	setNewUrlParameter(urlInput.value.toLowerCase());
+
+	fetchKeywords(urlValue, () => {
+		urlInput.classList.remove("loading");
+	});
+};
+
+const clearErrorStatus = () => {
+	const urlInput = document.querySelector("input[name=url]");
+
+	if(!urlInput.value) {
+		urlInput.classList.remove("error");
+		setNewUrlParameter('');
+	}
 };
 
 const start = () => {
 	document.querySelector("input[name=start]").addEventListener("click", startKeywordFinder);
+	document.querySelector("input[name=url]").addEventListener("blur", clearErrorStatus);
+	
+	const urlParams = new URLSearchParams(window.location.search);
+	const url = urlParams.get("url");
+
+	if(url) {
+		document.querySelector("input[name=url]").value = url;
+		startKeywordFinder();
+	}
 };
 
 document.addEventListener("DOMContentLoaded", start);
-
-
-
-let images = [
-	"https://www.peugeot.fr/content/dam/peugeot/france/b2c/odm/2022/octobre-2022/pac-2/mmt-3-colonnes/208_MultimediaTeaser_D_860x1000_SALON-PEUGE.jpg?imwidth=991",
-	"https://www.peugeot.fr/content/dam/peugeot/france/b2c/odm/2022/octobre-2022/pac-2/mmt-3-colonnes/2008_MultimediaTeaser_D_860x1000_SALON-PEUGE.jpg?imwidth=991",
-	"https://www.peugeot.fr/content/dam/peugeot/france/b2c/odm/2022/juillet-2022/e208/PEUGEOT_e208_2022_042_1214x1020.jpg?imwidth=991"
-];
-
-images.forEach(value => {
-	let img = document.createElement("img");
-	img.src = value;
-})
-
-
